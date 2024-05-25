8ed3e612db02d947f59053636571542ed0c5df8f
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-26 00:38:42 +0530

    basic css flex box

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  // parent flex with three children right next to ecah other
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{display:'flex'}}>
+
+      <div style={{backgroundColor:'red'}}>
+        HITHERE  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div style={{backgroundColor:'green'}}>
+        HITHERE
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div style={{backgroundColor:'pink'}}>
+        HITHERE
+      </div>
+
+    </div>
+
+
+    {/* parent with three childer right next to each other palced in end */}
+    <div style={{display:'flex',justifyContent:'flex-end'}}>
+
+        <div style={{backgroundColor:'red'}}>
+          HITHERE  
+        </div>
+      
+        <div style={{backgroundColor:'green'}}>
+          HITHERE
+        </div>
+         
+        <div style={{backgroundColor:'pink'}}>
+          HITHERE
+        </div>
+      
+    </div>
+   
     </>
   )
 }
